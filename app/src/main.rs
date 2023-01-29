75981380939f6010c6ab8ed0115d20deea9000b8
nickkane999 <nickkane999@gmail.com>
2023-01-29 12:50:38 -0500

    Lib is saved progress. Reverted back to main.rs, trying to get a function call to create labels so code is more condensed

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,5 @@
-use druid::{Data, widget::{Button, Flex, Label, TextBox}, AppLauncher, Env, WindowDesc, Widget, WidgetExt, Lens};
+use druid::{Data, widget::{Button, Flex, Label, Padding, Align, TextBox}, TextAlignment, theme, FontWeight,
+ AppLauncher, Env, WindowDesc, Widget, WidgetExt, Lens, FontDescriptor, FontFamily, kurbo::Insets};
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -11,19 +12,42 @@ struct FunnyData {
 }
 
 fn ui_builder() -> impl Widget<FunnyData> {
+    //const FONT_SIZE : Key<f64> = Key::new("org.linebender.druid.theme.text_size_large");
+    //println!("My font size is: {:?}", theme::TEXT_SIZE_LARGE);
+
+    let font_label = FontDescriptor::new(FontFamily::SYSTEM_UI)
+        .with_weight(FontWeight::BOLD)
+        .with_size(24.0);
+
     let count_label = Label::new(|data: &FunnyData, _: &Env| format!("Counter: {}", data.num));
     let increment = Button::new("+").on_click(|ctx, data: &mut FunnyData, _: &Env| data.num += 1);
     let decrement = Button::new("-").on_click(|ctx, data: &mut FunnyData, _: &Env| data.num -= 1);
 
-    let email_address_label = Label::new(|data: &FunnyData, _: &Env| format!("Email Address: "));
-    let email_address = TextBox::new().with_placeholder("Email Address").lens(FunnyData::email_address);
-    let email_subject_label = Label::new(|data: &FunnyData, _: &Env| format!("Email Subject: "));
-    let email_subject = TextBox::new().with_placeholder("Email Subject").lens(FunnyData::email_subject);
-    let email_body_label = Label::new(|data: &FunnyData, _: &Env| format!("Email Body: "));
-    let email_body = TextBox::new().with_placeholder("Email Body").lens(FunnyData::email_body);
+    // Width padding -> (left, top, right, bottom)
+    let email_address_label = Padding::new((0.0, 20.0, 0.0, 10.0), 
+    Align::left(Label::new(|data: &FunnyData, _: &Env| format!("Email Address: "))
+            .with_font(font_label.clone())));
+    let email_address = Padding::new((0.0, 0.0, 0.0, 20.0), 
+        TextBox::new().with_placeholder("Email Address").lens(FunnyData::email_address));
+    let email_subject_label = Padding::new((0.0, 0.0, 0.0, 10.0),
+        Label::new(|data: &FunnyData, _: &Env| format!("Email Subject: "))
+            .with_font(font_label.clone()));
+    let email_subject = Padding::new((0.0, 0.0, 0.0, 20.0), 
+        TextBox::new().with_placeholder("Email Subject")
+            .lens(FunnyData::email_subject));
+    let email_body_label = Padding::new((0.0, 0.0, 0.0, 10.0),
+        Label::new(|data: &FunnyData, _: &Env| format!("Email Body: "))
+            .with_font(font_label.clone()));
+    let email_body = Padding::new((0.0, 0.0, 00.0, 20.0),
+        TextBox::multiline()
+            .with_line_wrapping(true)
+            .with_text_alignment(TextAlignment::Start)
+            .with_text_size(theme::TEXT_SIZE_NORMAL)
+            .with_placeholder("Email Body")
+            .lens(FunnyData::email_body));
+
 
     let save = Button::new("Save").on_click(|ctx, data: &mut FunnyData, _: &Env| save_data(&data));
-
 
     Flex::column().with_child(
         Flex::column()
